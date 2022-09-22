08a3af1b8976110d40150c572a2c4dde9070a033
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-22 12:55:51 +0300

    Create BookStoreDbContext.cs

---
+++ BookStoreWebApi/DbOperations/BookStoreDbContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreWebApi.DbOperations
+{
+
+    public class BookStoreDbContext : DbContext
+    {
+
+        public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+
+    }
+}
