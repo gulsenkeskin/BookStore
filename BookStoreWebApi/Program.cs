7ffcd3df9da8e3ba87ec2ff13fbe9fc39d276917
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-22 15:09:39 +0300

    ef core implementation

--- BookStoreWebApi/Program.cs
+++ BookStoreWebApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using BookStoreWebApi.DbOperations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<BookStoreDbContext>(options => options.UseInMemoryDatabase(databaseName: "BookStoreDB"));
 
 var app = builder.Build();
 
