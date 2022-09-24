f247bfc70cf70f75a164e768ac422bc83737c0b9
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-24 13:17:59 +0300

    Update CreateBookCommandValidator.cs

--- BookStoreWebApi/BookOperations/CreateBook/CreateBookCommandValidator.cs
+++ BookStoreWebApi/BookOperations/CreateBook/CreateBookCommandValidator.cs
@@ -11,6 +11,11 @@ namespace BookStoreWebApi.BookOperations.CreateBook
         {
             //Genre ID sıfırdan büyük olmalı
             RuleFor(command => command.Model.GenreId).GreaterThan(0);
+            RuleFor(command => command.Model.PageCount).GreaterThan(0);
+            //tarih boş olmasın ve bu günden daha küçük olsun
+            RuleFor(command => command.Model.PublishDate.Date).NotEmpty().LessThan(DateTime.Now.Date);
+            //kitabın title ı boş olmasın ve miniömum 2 karakter olsun
+            RuleFor(command => command.Model.Title).NotEmpty().MinimumLength(2);
 
 
         }
