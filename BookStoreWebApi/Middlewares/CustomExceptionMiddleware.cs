5f2b04158dd1fae63e2473757c12a96af4388abf
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-27 16:08:53 +0300

    hata yakalama ve llog

--- BookStoreWebApi/Middlewares/CustomExceptionMiddleware.cs
+++ BookStoreWebApi/Middlewares/CustomExceptionMiddleware.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Http;
+using System.Net;
+using Newtonsoft.Json;
 
 namespace BookStoreWebApi.Middlewares
 {
@@ -13,25 +15,53 @@ namespace BookStoreWebApi.Middlewares
 
         public async Task Invoke(HttpContext context)
         {
-
             //requeste girip çıktığı süreyi hesaplamak için
             var watch = Stopwatch.StartNew();
+            try
+            {
+                //request log
+                string message = "[Request]  HTTP" + context.Request.Method + " - " + context.Request.Path;
+                Console.WriteLine(message);
 
-            //request log
-            string message = "[Request] HTTP" + context.Request.Method + " - " + context.Request.Path;
-            Console.WriteLine(message);
+                //bi sonraki middleware i çağırmak 
+                await _next(context);
+                watch.Stop();
 
-            //bi sonraki middleware i çağırmak 
-            await _next(context);
-            watch.Stop();
+                //response log
+                message = "[Response] HTTP " + context.Request.Method + " - " + context.Request.Path + " responded" + context.Response.StatusCode + " in" + watch.Elapsed.TotalMilliseconds + "ms";
+                Console.WriteLine(message);
+            }
+            catch (System.Exception ex)
+            {
+                //hata alırsa _next() den sonra stop'u çalıştıramayacağı için burda stop methodunu çağırırız.
 
-            //response log
-            message = "[Response] HTTP " + context.Request.Method + " - " + context.Request.Path + " responded" + context.Response.StatusCode + " in" + watch.Elapsed.TotalMilliseconds + "ms";
-            Console.WriteLine(message);
+                watch.Stop();
+                await HandleException(context, ex, watch);
+
+            }
+
+
 
 
         }
 
+        private Task HandleException(HttpContext context, Exception ex, Stopwatch watch)
+        {
+            //hata durumunda geriye dönülecek ve log'a yazılacak mesaj
+            string message = "[Error]   HTTP " + context.Request.Method + " - " + context.Response.StatusCode + "Error Message" + ex.Message + " in" + watch.Elapsed.TotalMilliseconds + " ms";
+
+            Console.WriteLine(message);
+
+            //Hata mesajını anlamlı bir şekilde dönebilmek için context'e yazarız.
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = Convert.ToInt32(HttpStatusCode.InternalServerError);
+
+            //json serileştirme için Newtonsoft paketini kullanırız
+            var result = JsonConvert.SerializeObject(new { error = ex.Message }, Formatting.None);
+
+            //geri dönmek için result'ı context içerisine yazarız
+            return context.Response.WriteAsync(result);
+        }
     }
 
     //program.js içerisinde bu middleware i app.use diye kullanabilmek için extension yazmamız gerekir.
