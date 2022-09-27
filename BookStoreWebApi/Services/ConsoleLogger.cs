f3536c1db804b7aaacf5cf817c3b5a6013956b45
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-27 20:29:55 +0300

    added logger service

---
+++ BookStoreWebApi/Services/ConsoleLogger.cs
@@ -0,0 +1,11 @@
+namespace BookStoreWebApi.Services
+{
+
+    public class ConsoleLogger : ILoggerService
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine("[Console Logger] - " + message);
+        }
+    }
+}
