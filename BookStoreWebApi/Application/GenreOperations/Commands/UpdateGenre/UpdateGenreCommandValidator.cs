8c27fe7aed3150b33d35559d707b319b77b1c294
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-28 15:28:28 +0300

    Update UpdateGenreCommandValidator.cs

--- BookStoreWebApi/Application/GenreOperations/Commands/UpdateGenre/UpdateGenreCommandValidator.cs
+++ BookStoreWebApi/Application/GenreOperations/Commands/UpdateGenre/UpdateGenreCommandValidator.cs
@@ -1,4 +1,14 @@
+using FluentValidation;
+
 namespace BookStoreWebApi.Application.GenreOperations.Commands.UpdateGenre
 {
+    public class UpdateGenreCommandValidator : AbstractValidator<UpdateGenreCommand>
+    {
+        public UpdateGenreCommandValidator()
+        {
+            RuleFor(command => command.GenreId).GreaterThan(0);
+            RuleFor(command => command.Model.Name).NotEmpty().MinimumLength(2);
+        }
+    }
 
 }
