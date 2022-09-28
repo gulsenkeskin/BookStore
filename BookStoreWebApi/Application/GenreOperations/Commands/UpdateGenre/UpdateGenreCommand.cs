f8a15a31e52603ed9958680f366fce73ea28c545
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-28 15:26:11 +0300

    Update UpdateGenreCommand.cs

--- BookStoreWebApi/Application/GenreOperations/Commands/UpdateGenre/UpdateGenreCommand.cs
+++ BookStoreWebApi/Application/GenreOperations/Commands/UpdateGenre/UpdateGenreCommand.cs
@@ -1,4 +1,39 @@
+using BookStoreWebApi.DbOperations;
+
 namespace BookStoreWebApi.Application.GenreOperations.Commands.UpdateGenre
 {
+    public class UpdateGenreCommand
+    {
+        private readonly BookStoreDbContext _context;
+
+        public UpdateGenreModel Model { get; set; }
+
+        public int GenreId { get; set; }
+
+        public UpdateGenreCommand(BookStoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Handle()
+        {
+            var genre = _context.Genres.SingleOrDefault(x => x.Id == GenreId);
+
+            if (genre is null)
+                throw new InvalidOperationException("Güncellenecek kategori bulunamadı");
+
+            genre.Name = Model.Name != default ? Model.Name : genre.Name;
+
+            _context.SaveChanges();
+        }
+
+    }
+
+    public class UpdateGenreModel
+    {
+        public string Name { get; set; }
+    }
+
+
 
 }
