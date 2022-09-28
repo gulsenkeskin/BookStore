a1ccdea3ade4e2bb7663e5cb89aa27f2ab3f2a12
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-28 13:03:46 +0300

    Update GetGenresQuery.cs

--- BookStoreWebApi/Application/GenreOperations/Queries/GetGenres/GetGenresQuery.cs
+++ BookStoreWebApi/Application/GenreOperations/Queries/GetGenres/GetGenresQuery.cs
@@ -1,7 +1,17 @@
+using AutoMapper;
+using BookStoreWebApi.DbOperations;
+
 namespace BookStoreWebApi.Application.GenreOperations.Queries
 {
     public class GetGenresQuery
     {
+        public readonly BookStoreDbContext _context;
+        public readonly IMapper _mapper;
+        public GetGenresQuery(BookStoreDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
 
     }
 }
