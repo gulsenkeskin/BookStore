2bc95888d889438a0c6c62588624303f0a055493
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-30 18:46:19 +0300

    Update CreateAuthorCommandValidator.cs

--- BookStoreWebApi/Application/AuthorOperations/Commands/CreateAuthor/CreateAuthorCommandValidator.cs
+++ BookStoreWebApi/Application/AuthorOperations/Commands/CreateAuthor/CreateAuthorCommandValidator.cs
@@ -1,4 +1,11 @@
+using FluentValidation;
+
+
 namespace BookStoreWebApi.Application.AuthorOperations.Commands.CreateAuthor
 {
+    public class CreateAuthorCommandValidator : AbstractValidator<CreateAuthorCommand>
+    {
+        RuleFor(command => command.Model.Name).NotEmpty().MinimumLength(2);
+    }
 
 }
