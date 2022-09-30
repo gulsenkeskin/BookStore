eb0f72b98db274b782a3326cc6a23787aca08f61
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-30 18:57:38 +0300

    Update DeleteAuthorCommand.cs

--- BookStoreWebApi/Application/AuthorOperations/Commands/DeleteAuthor/DeleteAuthorCommand.cs
+++ BookStoreWebApi/Application/AuthorOperations/Commands/DeleteAuthor/DeleteAuthorCommand.cs
@@ -1,4 +1,29 @@
+using BookStoreWebApi.DbOperations;
+
 namespace BookStoreWebApi.Application.AuthorOperations.Commands.DeleteAuthor
 {
+    public class DeleteAuthorCommand
+    {
+        private readonly BookStoreDbContext _context;
+        public int AuthorId { get; set; }
+
+        public DeleteAuthorCommand(BookStoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Handle()
+        {
+            var author = _context.Authors.SingleOrDefault(x => x.Id == AuthorId);
+
+            if (author is null)
+                throw new InvalidOperationException("Silinecek yazar bulunamadı");
+
+            _context.Authors.Remove(author);
+            _context.SaveChanges();
+        }
+
+
+    }
 
 }
