f8237d763c28692c24c121c744ae6496c364a1b0
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-10-03 18:41:45 +0300

    Update GetAuthorQuery.cs

--- BookStoreWebApi/Application/AuthorOperations/Queries/GetAuthors/GetAuthorQuery.cs
+++ BookStoreWebApi/Application/AuthorOperations/Queries/GetAuthors/GetAuthorQuery.cs
@@ -1,4 +1,34 @@
+using AutoMapper;
+using BookStoreWebApi.DbOperations;
+
 namespace BookStoreWebApi.Application.AuthorOperations.Queries.GetAuthor
 {
+    public class GetAuthorQuery
+    {
+        public readonly BookStoreDbContext _context;
+        public readonly IMapper _mapper;
+        public GetAuthorQuery(BookStoreDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public List<AuthorViewModel> Handle()
+        {
+            var author = _context.Authors.OrderBy(x => x.Id);
+            List<AuthorViewModel> returnObj = _mapper.Map<List<AuthorViewModel>>(author);
+            return returnObj;
+        }
+
+
+    }
+
+    public class AuthorViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
 
 }
