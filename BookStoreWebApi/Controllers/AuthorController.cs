d33514224cb4f9c06fbbbf8c21f58c4c45cadb01
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-10-03 21:11:54 +0300

    Update AuthorController.cs

---
+++ BookStoreWebApi/Controllers/AuthorController.cs
@@ -0,0 +1,90 @@
+using AutoMapper;
+using BookStoreWebApi.Application.AuthorOperations.Commands.CreateAuthor;
+using BookStoreWebApi.Application.AuthorOperations.Commands.DeleteAuthor;
+using BookStoreWebApi.Application.AuthorOperations.Commands.UpdateAuthor;
+using BookStoreWebApi.Application.AuthorOperations.Queries.GetAuthor;
+using BookStoreWebApi.Application.AuthorOperations.Queries.GetAuthorDetail;
+using BookStoreWebApi.DbOperations;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreWebApi.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]s")]
+    public class AuthorController : ControllerBase
+    {
+        private readonly BookStoreDbContext _context;
+        private readonly IMapper _mapper;
+        public AuthorController(BookStoreDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult GetAuthors()
+        {
+            GetAuthorQuery query = new GetAuthorQuery(_context, _mapper);
+            var obj = query.Handle();
+            return Ok(obj);
+        }
+
+        [HttpGet]
+        public ActionResult GetAuthorDetail(int id)
+        {
+            GetAuthorDetailQuery query = new GetAuthorDetailQuery(_context, _mapper);
+            query.AuthorId = id;
+            GetAuthorDetailQueryValidator validator = new GetAuthorDetailQueryValidator();
+            validator.ValidateAndThrow(query);
+
+            var obj = query.Handle();
+            return Ok(obj);
+        }
+
+        [HttpPost]
+        public IActionResult AddAuthor([FromBody] CreateAuthorModel newAuthor)
+        {
+            CreateAuthorCommand command = new CreateAuthorCommand(_context, _mapper);
+            command.Model = newAuthor;
+
+            CreateAuthorCommandValidator validator = new CreateAuthorCommandValidator();
+            validator.ValidateAndThrow(command);
+
+            command.Handle();
+            return Ok();
+        }
+
+
+        [HttpPost("id")]
+        public IActionResult UpdateAuthor(int id, [FromBody] UpdateAuthorModel updateAuthor)
+        {
+            UpdateAuthorCommand command = new UpdateAuthorCommand(_context);
+            command.AuthorId = id;
+            command.Model = updateAuthor;
+
+            UpdateAuthorCommandValidator validator = new UpdateAuthorCommandValidator();
+            validator.ValidateAndThrow(command);
+
+            command.Handle();
+            return Ok();
+        }
+
+        [HttpDelete("id")]
+        public IActionResult DeleteAuthor(int id)
+        {
+            DeleteAuthorCommand command = new DeleteAuthorCommand(_context);
+            command.AuthorId = id;
+
+            DeleteAuthorCommandValidator validator = new DeleteAuthorCommandValidator();
+            validator.ValidateAndThrow(command);
+
+            command.Handle();
+            return Ok();
+        }
+
+
+    }
+
+}
