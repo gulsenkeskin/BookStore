af62fc001c53f06950fd8ea0620925d30889a8e3
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-10-05 09:49:40 +0300

    User entiti

--- BookStoreWebApi/DbOperations/BookStoreDbContext.cs
+++ BookStoreWebApi/DbOperations/BookStoreDbContext.cs
@@ -15,6 +15,7 @@ namespace BookStoreWebApi.DbOperations
         public DbSet<Book> Books { get; set; }
         public DbSet<Genre> Genres { get; set; }
         public DbSet<Author> Authors { get; set; }
+        DbSet<User> Users { get; set; }
 
         public override int SaveChanges()
         {
